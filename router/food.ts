5e96e6dd5142eab1863b6069783b1612a2761fc7
Boldchingis <bboldchingis@gmail.com>
2025-01-17 12:41:17 +0800

    back

--- router/food.ts
+++ router/food.ts
@@ -1,7 +1,20 @@
 import { Router } from "express";
+import { model, Schema } from "mongoose";
+
+const FOOD_SCHEMA = new Schema(
+  {
+    foodName: String,
+    image: String,
+    price: Number,
+    ingredients: String,
+  },
+  { timestamps: true }
+);
+
 export const foodRouter = Router();
 foodRouter.get("/", async (req, res) => {
+  const foodModel = model("Food", FOOD_SCHEMA, "food")
   res.send({
-    message: "Tsuiwan, buuz ",
+    message: "hool nemegdlee ",
   });
 });
