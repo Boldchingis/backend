85646139d27627e3802054510aa9d3d7df41c7a8
Boldchingis <your-email@example.com>
2025-01-22 14:20:02 +0800

    Your commit message

--- router/food-category.ts
+++ router/food-category.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { FoodCategoryModel } from "../schmea/food-category";
+import { FoodCategoryModel } from "../Schmea/food-category";
 
 export const foodCategoryRouter = Router();
 
